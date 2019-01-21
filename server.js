6544d71560a9f2afa5610cedb3b13ece9bf354c6
Emily Dalton <emilydalton@Emilys-MacBook-Air.local>
2019-01-21 16:52:50 -0600

    adding node modules and requiring express in server.js file

---
+++ server.js
@@ -0,0 +1,8 @@
+//MVC stands for "model", "view", "controller" a pattern for organizing your code 
+// M stands for model. Represents your live model of your data 
+// View displaying the data 
+// Controllers Handle input for your database
+
+//requiring express
+var express = require("express");
+//
