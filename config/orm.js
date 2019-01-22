1c38e3a063fe34ca984adf965c097a2f1aa3aa1b
Emily Dalton <emilydalton@dhcp-10-105-17-25.wireless.northwestern.private>
2019-01-22 09:37:16 -0600

    orm file created, orm object exported

---
+++ config/orm.js
@@ -0,0 +1,73 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+// Helper function for SQL syntax.
+// Let's say we want to pass 3 values into the mySQL query.
+// In order to write the query, we need 3 question marks.
+// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
+// ["?", "?", "?"].toString() => "?,?,?";
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
+
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+  var arr = [];
+
+  // loop through the keys and push the key/value as a string int arr
+  for (var key in ob) {
+    var value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+      // e.g. {sleepy: true} => ["sleepy=true"]
+      arr.push(key + "=" + value);
+    }
+  }
+
+  // translate array of strings to a single comma-separated string
+  return arr.toString();
+}
+
+// Object for all our SQL statement functions is var orm
+// has a number of different methods on it 
+var orm = {
+//all method gets all of the data
+// the method takes in a table name and a callback
+// this method should be named selectAll per assignment requirements
+  all: function(tableInput, cb) {
+// this is where you get data from the database     
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+// once you get that data from the database, you have to send it back 
+// this data gets "pushed back up the chain" into our cat.js file    
+      cb(result);
+
+//the whole statement should end with these three sets of brances       
+    });
+  }
+//---------------------------------//
+//method for create will go here. This will be called insertOne.
+//---------------------------------//
+
+//---------------------------------//
+//method for update will go here. This will be called updateOne.
+//---------------------------------//
+
+//brace below ends the orm object  
+};
+// Export the orm object for the model (cat.js).
+module.exports = orm;
