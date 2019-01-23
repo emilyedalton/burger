48819e2b7859c1ef949648f7f9fd9fb1adb61b4f
Emily Dalton <emilydalton@dhcp-10-105-17-25.wireless.northwestern.private>
2019-01-23 14:54:50 -0600

    folder structure in public correction

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,14 +1,15 @@
 
+  $(function() {
   $(".create-form").on("submit", function(event) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
     var newburger = {
-      burger_name: $("#burg").val().trim(),
+      burger_name: $("#burg").val().trim()
     };
 
     // Send the POST request.
-    $.ajax("/api/burger", {
+    $.ajax("/burger/new", {
       type: "POST",
       data: newburger
     }).then(
@@ -16,6 +17,6 @@
         console.log("created new burger");
         // Reload the page to get the updated list
         location.reload();
-      }
-    );
+      });
+    });
   });
